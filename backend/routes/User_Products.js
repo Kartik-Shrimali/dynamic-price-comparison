f27899e714f181cd3938274997c1fe6432c849ae
Kartik Shrimali <kartikshrimali62@gmail.com>
2025-03-16 01:08:57 +0530

    completed basic apis for products

--- backend/routes/User_Products.js
+++ backend/routes/User_Products.js
@@ -5,7 +5,7 @@ const { JWT_SECRET } = require("../JWT_SECRET")
 const jwt = require("jsonwebtoken");
 const authMiddleware = require("../middleware")
 
-router.get("/", authMiddleware , async (req, res) => {
+router.get("/", authMiddleware, async (req, res) => {
     try {
         const Pid = parseInt(req.query.id);
 
@@ -26,7 +26,7 @@ router.get("/", authMiddleware , async (req, res) => {
         }
 
         if (Pid && products.length === 0) {
-            return res.status(400).json({
+            return res.status(404).json({
                 msg: "Product not found"
             })
         }
@@ -41,17 +41,77 @@ router.get("/", authMiddleware , async (req, res) => {
     }
 })
 
-router.get("/prices" , authMiddleware , async (req , res)=>{
-    // const ProductId = parseInt(req.query.id);
+router.get("/prices", authMiddleware, async (req, res) => {
+    const ProductId = parseInt(req.query.id);
 
-    // if(!ProductId){
-    //     return res.json({
-    //         msg : "provide a product id"
-    //     })
-    // }
+    try {
+        if (!ProductId) {
+            return res.status(400).json({
+                msg: "provide a product id"
+            })
+        }
 
-    // await pool.query("SELECT ")
+        let query = `SELECT stores.name as store_name,
+                                prices.price,
+                                prices.availability,
+                                prices.updated_at 
+                          FROM prices
+                          JOIN stores ON prices.store_id = stores.id
+                          WHERE prices.product_id = ?
+                          ORDER BY prices.price ASC`;
+
+        let [response] = await pool.query(query, [ProductId]);
+
+        if (response.length === 0) {
+            return res.status(404).json({
+                msg: "Product not found"
+            })
+        }
+
+        return res.status(200).json(response);
+    } catch (err) {
+        return res.status(500).json({
+            msg: "There was some intetrnal error",
+            error : err.message
+        })
+    }
+
 })
 
+router.get("/availability", authMiddleware, async (req, res) => {
+    let ProductId = parseInt(req.query.id);
+
+    try {
+        if (!ProductId) {
+            return res.status(400).json({
+                msg: "No id is provided.Please provide id"
+            })
+        }
+
+        let query = `SELECT stores.name as store_name,
+                    prices.availability,
+                    prices.updated_at
+                    FROM prices
+                    JOIN stores ON prices.store_id = stores.id
+                    WHERE prices.product_id = ? AND prices.availability = 1`;
+
+        let [response] = await pool.query(query , [ProductId]);
+
+        if(response.length === 0){
+            return res.status(404).json({
+                msg : "The product is not available OR out of stock"
+            })
+        }
+
+        return res.status(200).json(response);
+
+    } catch (err) {
+        return res.status(500).json({
+            msg : "There was some error in server",
+            error : err.message
+        })
+    }
+
+})
 
 module.exports = router
