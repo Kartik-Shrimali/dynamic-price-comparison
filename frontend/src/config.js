75e2d18569e04e505af216b19289e6dda5b83ee2
Kartik-Shrimali <kartikshrimali62@gmail.com>
2025-11-28 23:59:52 +0530

    FIX: Updated frontend API URL to use ALB DNS name.

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,2 @@
-const BACKEND_API_BASE_URL = 'http://localhost:3000'; 
+const BACKEND_API_BASE_URL = 'http://price-wise-backend-alb-1069619239.ap-south-1.elb.amazonaws.com'; 
 export { BACKEND_API_BASE_URL };
