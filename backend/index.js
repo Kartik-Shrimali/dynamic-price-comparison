d6a6df4c5f374b0754e14324d732ebf94fc16b4f
Kartik-Shrimali <kartikshrimali62@gmail.com>
2025-11-30 00:43:38 +0530

    Final : health check api added on /

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,13 @@ app.use(express.json());
 
 app.use("/api/v1" , Router );
 
+app.get('/', (req, res) => {
+    res.status(200).send({
+        status: "API is running",
+        version: "v1"
+    });
+});
+
 app.get('/health', (req, res) => {
     res.status(200).send('OK');
 });
